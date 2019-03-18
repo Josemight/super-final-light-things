bc234f49babf0e8f354654b48fab8a0345a4de2d
Victor Baquero Wihlborg <jag@vbw.se>
2019-03-18 12:22:56 +0100

    Add theme buttons

--- prototypes/light-control/static/control/control.js
+++ prototypes/light-control/static/control/control.js
@@ -1,5 +1,29 @@
 const sliders = [...document.querySelectorAll(".Slider")];
 const controlLight = document.querySelector(".Control-light");
+const themeBtns = [...document.querySelectorAll(".Control-themeButton")];
+
+const themes = [
+  {
+    hue: 44,
+    saturation: 100,
+    lightness: 26
+  },
+  {
+    hue: 44,
+    saturation: 12,
+    lightness: 75
+  },
+  {
+    hue: 314,
+    saturation: 100,
+    lightness: 50
+  },
+  {
+    hue: 85,
+    saturation: 100,
+    lightness: 42
+  }
+];
 
 let lightData = {
   hue: 48,
@@ -11,9 +35,16 @@ const handleSliderInput = ev => {
   sliders.forEach(slider => {
     lightData[ev.target.name] = Number(ev.target.value);
   });
-  controlLight.style.backgroundColor = `hsl(${lightData.hue}, ${
-    lightData.saturation
-  }%, ${lightData.lightness}%)`;
+  setVisual();
+};
+
+const handleThemeClick = themeId => {
+  const newTheme = themes[themeId];
+  console.log(newTheme);
+  lightData = Object.assign({}, newTheme);
+  setSliders();
+  setVisual();
+  sendColor();
 };
 
 const setSliders = () => {
@@ -22,13 +53,22 @@ const setSliders = () => {
   });
 };
 
+const setVisual = () => {
+  controlLight.style.backgroundColor = `hsl(${lightData.hue}, ${
+    lightData.saturation
+  }%, ${lightData.lightness}%)`;
+};
+
 const handleSliderChange = ev => {
   let change = {};
   sliders.forEach(slider => {
     change[ev.target.name] = Number(ev.target.value);
   });
+  sendColor();
+};
+const sendColor = () => {
   try {
-    socket.send(JSON.stringify(change));
+    socket.send(JSON.stringify(lightData));
   } catch (err) {
     logError(err);
   }
@@ -40,6 +80,11 @@ window.onload = () => {
   });
   sliders.forEach(slider => {
     slider.addEventListener("input", handleSliderInput);
+  });
+  themeBtns.forEach((slider, i) => {
+    slider.addEventListener("click", () => {
+      handleThemeClick(i);
+    });
   });
   setSliders();
 };
